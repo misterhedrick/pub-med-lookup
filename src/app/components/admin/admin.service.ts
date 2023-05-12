be3774f6abc9b05aa1cd3578d9fa90c1cc095c27
Daniel Hedrick <misterhedrick@gmail.com>
2023-05-12 06:15:22 -0400

    css cleanup

--- src/app/components/admin/admin.service.ts
+++ src/app/components/admin/admin.service.ts
@@ -11,16 +11,26 @@ import {
 
 import { Firestore, collectionData, docData } from '@angular/fire/firestore';
 import { Folder } from '../../models/folder';
-import { Observable } from 'rxjs';
+import { Observable, from, startWith } from 'rxjs';
+import { StorageReference, getDownloadURL } from '@angular/fire/storage';
+import { getStorage, ref } from '@firebase/storage';
+import { keepUnstableUntilFirst } from '@angular/fire';
+import { traceUntilFirst } from '@angular/fire/performance';
+
+const TRANSPARENT_PNG = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminService {
   private folderCollection: CollectionReference<DocumentData>;
+  storageRef: StorageReference;
   folder: Folder;
   folders: Folder[] = [];
-  constructor(private readonly firestore: Firestore) {}
+  constructor(private readonly firestore: Firestore) {
+    this.storageRef = ref(getStorage());
+  }
 
   setFolderCollection(uid: string) {
     this.folderCollection = collection(this.firestore, uid);
@@ -56,5 +66,18 @@ export class AdminService {
   deleteFolder(id: string) {
     const folderDocumentReference = doc(this.firestore, `folders/${id}`);
     return deleteDoc(folderDocumentReference);
+  }
+  getImageURL(filepath: string) {
+    const imgref = this.getImageRef(filepath);
+    const downloadUrl$ = from(getDownloadURL(imgref)).pipe(
+      keepUnstableUntilFirst,
+      traceUntilFirst('storage'),
+      startWith(TRANSPARENT_PNG),
+    );
+    console.log('downloadurl: ', downloadUrl$);
+    return downloadUrl$;
+  }
+  getImageRef(filepath: string) {
+    return ref(this.storageRef, filepath);
   }
 }
--- src/app/components/admin/details/details.component.ts
+++ src/app/components/admin/details/details.component.ts
@@ -10,24 +10,29 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class DetailsComponent {
   private readonly storage: Storage = inject(Storage);
+  images: any[] = [];
 
-  constructor(public adminService: AdminService, private authService: AuthService) {}
+  constructor(public adminService: AdminService, private authService: AuthService) {
+    for (var file of adminService.folder.files) {
+      this.images.push(this.adminService.getImageURL(file.fullpath));
+    }
+  }
 
   uploadFile(input: HTMLInputElement) {
     if (!input.files) return
-
     const files: FileList = input.files;
-
     for (let i = 0; i < files.length; i++) {
         const file = files.item(i);
         if (file) {
             const storageRef = ref(this.storage, `${this.authService.loggedInUser.uid}/${this.adminService.folder.name}/${file.name}`);
             uploadBytesResumable(storageRef, file).then((response) => {
-              const downloadURL = getDownloadURL(response.ref);
               this.adminService.folder.files.push({filename: response.metadata.name, fullpath: response.metadata.fullPath, thumbnail: ''});
               this.adminService.updateFolder(this.authService.loggedInUser.uid, this.adminService.folder);
             });
         }
     }
+}
+getImage(file: any) {
+  return this.adminService.getImageURL(file.fullpath);
 } 
 }
